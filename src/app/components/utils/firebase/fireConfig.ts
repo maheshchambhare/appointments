938b17e3574c995f4f003ec2a1ce1ca33115b0f0
Mahesh Chambhare <chambharem7@gmail.com>
2024-04-12 16:16:56 +0530

    service.json deleted

--- src/app/components/utils/firebase/fireConfig.ts
+++
@@ -1,9 +0,0 @@
-export const fireConfig = {
-  apiKey: "AIzaSyADXaFYBDikz_LFgRvlyRvyh-OK9IeHIN0",
-  authDomain: "appointify-e6cff.firebaseapp.com",
-  projectId: "appointify-e6cff",
-  storageBucket: "appointify-e6cff.appspot.com",
-  messagingSenderId: "175388342654",
-  appId: "1:175388342654:web:844a16e4c533508f571b2f",
-  measurementId: "G-2SFHLCMF19",
-};
